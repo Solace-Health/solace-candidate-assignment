c917920bf815b555acc90698dc672edb569be1f5
galenom <galeno.mario@gmail.com>
2024-08-07 23:04:09 -0400

    feat: move filtering logic to api endpoint
    this is preferred over receiving  the full list of advocates and filtering on the client side.

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,29 @@
+import { NextRequest } from 'next/server';
 import db from '../../../db';
 import { advocates } from '../../../db/schema';
 import { advocateData } from '../../../db/seed/advocates';
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   // Uncomment this line to use a database
   // const data = await db.select().from(advocates);
 
-  const data = advocateData;
+  const { searchParams } = req.nextUrl;
+
+  const searchTerm = searchParams.get('search')?.toLowerCase() || '';
+  console.log('api/search', searchTerm);
+
+  const data = advocateData.filter((advocate) => {
+    return (
+      advocate.firstName.toLowerCase().includes(searchTerm) ||
+      advocate.lastName.toLowerCase().includes(searchTerm) ||
+      advocate.city.toLowerCase().includes(searchTerm) ||
+      advocate.degree.toLowerCase().includes(searchTerm) ||
+      advocate.specialties
+        .map((s) => s.toLowerCase())
+        .filter((s) => s.includes(searchTerm)).length > 0 ||
+      advocate.yearsOfExperience === Number(searchTerm)
+    );
+  });
 
   return Response.json({ data });
 }
